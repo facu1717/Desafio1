ab7eb6574818dda6a4b715ff8ab4d3ead08e2679
facu1717 <56850265+facu1717@users.noreply.github.com>
2021-06-16 20:54:05 -0300

    Primer commit

---
+++ Entities/Suscriptor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Suscriptor
+    {
+        public int IdSuscriptor { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int NumeroDocumento { get; set; }
+        public int TipoDocumento { get; set; }
+        public string Direccion { get; set; }
+        public int Telefono { get; set; }
+        public string Email { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Password { get; set; }
+    }
+}
